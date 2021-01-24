b4764aa410d1108c1255bb2d63f28fe4c3b1389a
PeterXiang <565067150@qq.com>
2021-01-25 02:46:43 +0800

    save

--- Runtime/Utility/MathUtility.cs
+++ Runtime/Utility/MathUtility.cs
@@ -150,54 +150,6 @@ namespace XMLib
             return Mathf.Abs(left - right) < FEpsilon;
         }
 
-        #region byte 与 angle 的映射 ， 现在有bug
-
-        public const byte ByteAngleScale = 2;
-
-        /// <summary>
-        /// byte 映射到角度
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public static float ByteToAngle(byte value)
-        {
-            return value * ByteAngleScale;
-        }
-
-        public static byte AngleToByte(float angle)
-        {
-            angle = angle % 360f;
-            angle += angle >= 0 ? 0 : 360f;
-            return (byte)(angle == 0 ? 0 : (angle / ByteAngleScale));
-        }
-
-        public static float FixedByteAngle(float angle)
-        {
-            return ByteToAngle(AngleToByte(angle));
-        }
-
-        public static float AngleYFromDir(Vector3 dir)
-        {
-            dir.y = 0f;//在同一平面旋转
-            return Vector3.SignedAngle(Vector3.forward, dir, Vector3.up);
-        }
-
-        public static Vector3 AngleYToDir(float yAngle)
-        {
-            return (Quaternion.AngleAxis(yAngle, Vector3.up) * Vector3.forward).normalized;
-        }
-
-        public static byte ByteAngleYFromDir(Vector3 dir)
-        {
-            return AngleToByte(AngleYFromDir(dir));
-        }
-
-        public static float FixedByteAngleYFromDir(Vector3 dir)
-        {
-            return FixedByteAngle(AngleYFromDir(dir));
-        }
-
-        #endregion byte 与 angle 的映射 ， 现在有bug
 
         /// <summary>
         /// CalcBoxVertex 计算得到的顶点，绘制序列，4个为一个面，共6个面，顺序为上下左右前后
